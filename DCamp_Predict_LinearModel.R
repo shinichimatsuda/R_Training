f4b3d7f26726c416928a1e6876e80d645aa17cbf
SMatsuda <matsushin51@gmail.com>
2018-05-14 09:20:51 +0900

    Create DCamp_Predict_LinearModel.R

---
+++ DCamp_Predict_LinearModel.R
@@ -0,0 +1,10 @@
+# The Wage dataset is available
+
+# Build Linear Model: lm_wage (coded already)
+lm_wage <- lm(wage ~ age, data = Wage)
+
+# Define data.frame: unseen (coded already)
+unseen <- data.frame(age = 60)
+
+# Predict the wage for a 60-year old worker
+predict(lm_wage, unseen)

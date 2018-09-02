90f93d2006fd14385328e4fbce725792e188cd0b
SMatsuda <matsushin51@gmail.com>
2018-09-02 23:23:10 +0900

    Create Select data frame elements.R

---
+++ DCamp/Select data frame elements.R
@@ -0,0 +1,7 @@
+# The planets_df data frame from the previous exercise is pre-loaded
+
+# Print out diameter of Mercury (row 1, column 3)
+print(planets_df[1,3])
+
+# Print out data for Mars (entire fourth row)
+print(planets_df[4,])

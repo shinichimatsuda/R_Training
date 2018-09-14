7b277b0230ee9993ca65f07788ae661d97100b10
SMatsuda <matsushin51@gmail.com>
2018-09-15 05:54:30 +0900

    Create Loop over a vector 2 patterns.R

---
+++ DCamp/Intermediate R/Loop over a vector 2 patterns.R
@@ -0,0 +1,14 @@
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for (var in linkedin) {
+  print (var)
+}
+
+
+
+# Loop version 2
+for (i in 1:length(linkedin)) {
+  print(linkedin[i])
+}

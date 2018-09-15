e54c25495acead370c29f3297369706a850fe5ed
SMatsuda <matsushin51@gmail.com>
2018-09-16 07:28:11 +0900

    Create Creating a function.R

---
+++ DCamp/Intermediate R/Creating a function.R
@@ -0,0 +1,15 @@
+# Create a function pow_two()
+pow_two <- function(x) {
+  x^2
+}
+
+# Use the function
+pow_two(12)
+
+# Create a function sum_abs()
+sum_abs <- function(x, y) {
+  sum(abs(x), abs(y))
+}
+
+# Use the function
+sum_abs(-2, 3)

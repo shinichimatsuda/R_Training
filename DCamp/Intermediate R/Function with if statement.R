36430e70ef3d08ff72fb8ccb337f666e52c12d5a
SMatsuda <matsushin51@gmail.com>
2018-09-16 07:37:33 +0900

    Create Function with if statement.R

---
+++ DCamp/Intermediate R/Function with if statement.R
@@ -0,0 +1,8 @@
+# Finish the pow_two() function
+pow_two <- function(x, print_info = TRUE) {
+  y <- x ^ 2
+  if (print_info == TRUE) {
+  print(paste(x, "to the power two equals", y))
+  }
+  return(y)
+}

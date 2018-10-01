aff47bc38429bfb61c54a748fdceef9151090828
SMatsuda <matsushin51@gmail.com>
2018-10-01 17:02:03 +0900

    Create Adding a regression line manually.R

---
+++ DCamp/Correlation and Regression_R/SimpleLinearRegression/Adding a regression line manually.R
@@ -0,0 +1,6 @@
+# Add the line to the scatterplot
+ggplot(data = bdims, aes(x = hgt, y = wgt)) + 
+  geom_point() + 
+  geom_abline(data = coefs, 
+              aes(intercept = `(Intercept)`, slope = hgt),  
+              color = "dodgerblue")

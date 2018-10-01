ab6c6b0a3ebc116621a15af032c9fa8c50f69e0e
SMatsuda <matsushin51@gmail.com>
2018-10-02 06:23:45 +0900

    Create Creating a subgraph facet_wrap().R

---
+++ DCamp/Intro Tidyverse/Data Visualization/Creating a subgraph facet_wrap().R
@@ -0,0 +1,12 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Scatter plot comparing pop and lifeExp, faceted by continent
+ggplot(data = gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point() +
+  scale_x_log10() +
+  facet_wrap(~ continent)

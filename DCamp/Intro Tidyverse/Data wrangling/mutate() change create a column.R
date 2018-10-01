e889e8fd2bbb871b57e6a0b8d8ee6aa12256d790
SMatsuda <matsushin51@gmail.com>
2018-10-01 09:56:05 +0900

    Create mutate() change create a column.R

---
+++ DCamp/Intro Tidyverse/Data wrangling/mutate() change create a column.R
@@ -0,0 +1,10 @@
+library(gapminder)
+library(dplyr)
+
+# Use mutate to change lifeExp to be in months
+gapminder %>%
+  mutate(lifeExp = 12 * lifeExp)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>%
+  mutate(lifeExpMonths = 12 * lifeExp)

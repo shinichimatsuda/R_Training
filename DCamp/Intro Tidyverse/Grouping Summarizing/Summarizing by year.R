f2076b94cdf8f4eae9d5251864ab2b00c4feac50
SMatsuda <matsushin51@gmail.com>
2018-10-03 22:26:24 +0900

    Create Summarizing by year.R

---
+++ DCamp/Intro Tidyverse/Grouping Summarizing/Summarizing by year.R
@@ -0,0 +1,7 @@
+library(gapminder)
+library(dplyr)
+
+# Find median life expectancy and maximum GDP per capita in each year
+gapminder %>%
+  group_by(year) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))

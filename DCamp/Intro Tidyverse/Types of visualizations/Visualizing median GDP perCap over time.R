7bde39a1e18a9b2aee6a2dede171587b2be5dac3
SMatsuda <matsushin51@gmail.com>
2018-10-03 22:06:00 +0900

    Create Visualizing median GDP perCap over time.R

---
+++ DCamp/Intro Tidyverse/Types of visualizations/Visualizing median GDP perCap over time.R
@@ -0,0 +1,13 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Summarize the median gdpPercap by year, then save it as by_year
+by_year <- gapminder %>%
+  group_by(year) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a line plot showing the change in medianGdpPercap over time
+ggplot(data = by_year, aes(x = year, y = medianGdpPercap)) +
+  geom_line() +
+  expand_limits(y = 0)

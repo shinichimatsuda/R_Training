e3c4f920c9f8787a92a89626f5423fad42ffe06f
SMatsuda <matsushin51@gmail.com>
2018-10-03 22:33:30 +0900

    Create Summarizing by continent.R

---
+++ DCamp/Intro Tidyverse/Grouping Summarizing/Summarizing by continent.R
@@ -0,0 +1,8 @@
+library(gapminder)
+library(dplyr)
+
+# Find median life expectancy and maximum GDP per capita in each continent in 1957
+gapminder %>%
+  filter(year == 1957) %>%
+  group_by(continent) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))

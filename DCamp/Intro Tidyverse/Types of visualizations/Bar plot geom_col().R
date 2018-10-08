38265379b95a4f0ef3c9fbbdf501525bd7bba43e
SMatsuda <matsushin51@gmail.com>
2018-10-08 14:57:39 +0900

    Create Bar plot geom_col().R

---
+++ DCamp/Intro Tidyverse/Types of visualizations/Bar plot geom_col().R
@@ -0,0 +1,13 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Summarize the median gdpPercap by year and continent in 1952
+by_continent <- gapminder %>%
+  filter(year == 1952) %>%
+  group_by(continent) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a bar plot showing medianGdp by continent
+ggplot(data = by_continent, aes(x = continent, y = medianGdpPercap)) +
+  geom_col()

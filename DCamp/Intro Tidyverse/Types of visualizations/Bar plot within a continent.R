b2a12cd9610e6b1d54e19fe284faa7564ce937eb
SMatsuda <matsushin51@gmail.com>
2018-10-08 15:16:17 +0900

    Create Bar plot within a continent.R

---
+++ DCamp/Intro Tidyverse/Types of visualizations/Bar plot within a continent.R
@@ -0,0 +1,11 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Filter for observations in the Oceania continent in 1952
+oceania_1952 <- gapminder %>%
+  filter(continent == "Oceania", year == 1952)
+
+# Create a bar plot of gdpPercap by country
+ggplot(data = oceania_1952, aes(x = country, y = gdpPercap)) +
+  geom_col()

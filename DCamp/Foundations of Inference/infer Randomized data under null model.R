3236877611ea263ac62dae36fce67bf2fac4ea36
SMatsuda <matsushin51@gmail.com>
2018-10-13 04:01:06 +0900

    Create infer Randomized data under null model.R

---
+++ DCamp/Foundations of Inference/infer Randomized data under null model.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+library(NHANES)
+library(infer)
+
+# Perform 10 permutations
+homeown_perm <- homes %>%
+  specify(HomeOwn ~ Gender, success = "Own") %>%
+  hypothesize(null = "independence") %>% 
+  generate(reps = 10, type = "permute") 
+
+
+# Print results to console
+homeown_perm

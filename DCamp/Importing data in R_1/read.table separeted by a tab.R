55e5c02befa861de30613dec88b1be553a5e7119
SMatsuda <matsushin51@gmail.com>
2018-10-13 04:57:53 +0900

    Create read.table separeted by a tab.R

---
+++ DCamp/Importing data in R_1/read.table separeted by a tab.R
@@ -0,0 +1,10 @@
+# Path to the hotdogs.txt file: path
+path <- file.path("data", "hotdogs.txt")
+
+# Import the hotdogs.txt file: hotdogs
+hotdogs <- read.table(path, 
+                      sep = "\t", 
+                      col.names = c("type", "calories", "sodium"))
+
+# Call head() on hotdogs
+head(hotdogs)

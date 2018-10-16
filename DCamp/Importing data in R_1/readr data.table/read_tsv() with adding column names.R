c2b78182f13ff8a9e2f17d671e067d1e54d9637c
SMatsuda <matsushin51@gmail.com>
2018-10-17 05:15:50 +0900

    Create read_tsv() with adding column names.R

---
+++ DCamp/Importing data in R_1/readr data.table/read_tsv() with adding column names.R
@@ -0,0 +1,11 @@
+library(readr) # readr is already loaded
+
+# Column names
+properties <- c("area", "temp", "size", "storage", "method",
+                "texture", "flavor", "moistness")
+
+# Import potatoes.txt: potatoes
+potatoes <- read_tsv("potatoes.txt", col_names = properties)
+
+# Call head() on potatoes
+head(potatoes)

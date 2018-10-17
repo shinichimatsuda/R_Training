21c67d0bb75cb77ba6057d350be68a1b6efbd1d6
SMatsuda <matsushin51@gmail.com>
2018-10-18 06:29:44 +0900

    Create Matching %in%.R

---
+++ edX/DS R Basics/Indexing/Matching %in%.R
@@ -0,0 +1,9 @@
+# Store the 5 abbreviations in abbs. (remember that they are character vectors)
+abbs <- c("MA", "ME", "MI", "MO", "MU") 
+
+# Use the `which` command and `!` operator to find out which abbreviation are not actually part of the dataset and store in ind
+
+ind <- which(!abbs %in% murders$abb)
+
+# What are the entries of abbs that are not actual abbreviations
+abbs[ind]

9e2d9264ec5a0f9511aba24e49f7764cc7a445cd
SMatsuda <matsushin51@gmail.com>
2018-10-19 06:34:46 +0900

    Create dplyr introduction.R

---
+++ edX/DS R Basics/Basic Data Wrangling/dplyr introduction.R
@@ -0,0 +1,9 @@
+# Loading data
+library(dslabs)
+data(murders)
+
+# Loading dplyr
+library(dplyr)
+
+# Redefine murders so that it includes column named rate with the per 100,000 murder rates
+murders <- mutate(murders, rate = total / population * 10^5)

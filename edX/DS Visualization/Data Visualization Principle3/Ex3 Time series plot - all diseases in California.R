ba1713783b768b87f1eb60e0fd966e7ef0f0effa
SMatsu <matsushin51@gmail.com>
2018-12-15 00:18:19 +0900

    Create Ex3 Time series plot - all diseases in California.R

---
+++ edX/DS Visualization/Data Visualization Principle3/Ex3 Time series plot - all diseases in California.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+library(RColorBrewer)
+data(us_contagious_diseases)
+
+us_contagious_diseases %>%
+  filter(state == "California" & !weeks_reporting < 10) %>% 
+  group_by(year, disease) %>%
+  summarize(rate = sum(count)/sum(population)*10000) %>%
+  ggplot(aes(year, rate, color = disease)) + 
+  geom_line()

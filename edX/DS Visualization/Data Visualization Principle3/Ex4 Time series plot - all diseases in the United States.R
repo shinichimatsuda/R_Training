df9b30f2ff0886d1b6fa0ad6f3db71e018b7c24d
SMatsu <matsushin51@gmail.com>
2018-12-15 00:20:15 +0900

    Create Ex4 Time series plot - all diseases in the United States.R

---
+++ edX/DS Visualization/Data Visualization Principle3/Ex4 Time series plot - all diseases in the United States.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+library(RColorBrewer)
+data(us_contagious_diseases)
+
+us_contagious_diseases %>%
+  filter(!is.na(population)) %>% 
+  group_by(year, disease) %>%
+  summarize(rate = sum(count)/sum(population)*10000) %>%
+  ggplot(aes(year, rate, color = disease)) + 
+  geom_line()
